514fc0094ce970f9a93beee59c04687f7d64b84d
Prasad Jawale <80669588+Tydos@users.noreply.github.com>
2025-11-21 12:44:02 -0600

    configured github pages deployment scripts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const repoName = "adhd-task-manager";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
