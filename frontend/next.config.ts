3ebbb60c6bba1c85727fd7772bf193a5bea4cc9a
apurvab15 <apurvabasapure@gmail.com>
2025-12-03 13:18:22 -0600

    deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const repoName = "adhd-task-manager";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  //comment out basepath and assetprefix for local builds
+  //basePath: `/${repoName}`,
+  //assetPrefix: `/${repoName}/`,
+  trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
